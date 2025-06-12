92762fcb9a5f2ded185075f668f8d9d8051abcf4
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-06-12 12:36:20 +0600

    initial 2

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	connStr := "host=localhost port=5432 user=postgres password=password dbname=testdb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("DB connection failed:", err)
+	}
+	fmt.Println("Connected to DB!")
+
+	http.HandleFunc("/api/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong from backend"))
+	})
+
+	fmt.Println("Server running at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
